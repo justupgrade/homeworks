6f08b2585724130ae43299b18a32a6a3c415a37d
Tomasz <justupgrade@o2.pl>
2015-03-07 22:05:37 +0100

    Added main function for app: displaying 6 grids at once

--- h1_loops/loops.php
+++ h1_loops/loops.php
@@ -11,6 +11,26 @@
     require './classes/Grid.php';
     require './data/grid_data.php';
     
+    showAllGrids($hash_grids);
+    
+    function showAllGrids($grids) {
+        $grid = new Grid();
+        
+        echo "<pre>";
+        echo "<section>";
+        
+        for($i = 0; $i < count($grids); $i++) {
+            $grid->grid = $grids[$i];
+            echo "<div>";
+            $grid->displayGrid();
+            echo "</div>";
+            
+            if($i == 4) echo "</section><section>";
+        }
+        echo "</section>";
+        echo "</pre>";
+    }
+    
 ?>
 
 </body>
